643688d8e017887aedf1a3e566eaa3abebb49459
Ivy_Chiang <ivychiang1109@gmail.com>
2025-02-13 16:03:04 +0100

    e3

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get 'bookmarks/new'
-  get 'bookmarks/create'
-  get 'bookmarks/destroy'
   resources :lists, only: [:index, :show, :new, :create] do
     resources :bookmarks, only: [:new, :create]  # Nested since bookmarks belong to lists
   end
