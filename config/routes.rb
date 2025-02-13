ff527677c51020a016a618cf2de171030c48e84d
Ivy_Chiang <ivychiang1109@gmail.com>
2025-02-13 14:22:58 +0100

    e2

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  get 'bookmarks/new'
+  get 'bookmarks/create'
+  get 'bookmarks/destroy'
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]  # Nested since bookmarks belong to lists
+  end
+  resources :bookmarks, only: [:destroy]  # Direct delete route
 end
